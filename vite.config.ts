62cd105ad7933f8df6e1ea5a5115cc3b213efe00
hariharan-030708 <hariharan030708@gmail.com>
2026-08-11 22:23:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/HariharanRG-Portfolio',
+    base: '/HariharanRG-Portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
